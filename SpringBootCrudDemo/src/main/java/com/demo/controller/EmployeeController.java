ab5a7a681b4ac65213c5a04862c9619b848153d2
Raju Pawar <78150162+rajuPawar1234@users.noreply.github.com>
2022-06-01 19:51:04 +0530

    added request mapping

--- SpringBootCrudDemo/src/main/java/com/demo/controller/EmployeeController.java
+++ SpringBootCrudDemo/src/main/java/com/demo/controller/EmployeeController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.model.Employee;
 import com.demo.service.EmployeeServiceInterface;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class EmployeeController {
 
 	@Autowired
